89c7f40d0da7e62bae90e358e979992be79b1d2f
Abdelfatah Alhoot <abdelfatahalhoot@gmail.com>
2026-04-20 00:07:22 +0300

    Move client into GoGameClient and add GoGameServer
    fixed directory and added setup for server program

---
+++ GoGameServer/src/main/java/com/mycompany/gogameserver/GoGameServer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gogameserver;
+
+/**
+ *
+ * @author abdel
+ */
+public class GoGameServer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
